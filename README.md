50aaf1f778f502395155414ed2c3204958c33096
ricsilva <riccsilvaa@gmail.com>
2022-01-17 16:45:11 +0000

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Restaurant Page using HTML, CSS, Javascript and Webpack.
-
-<img src="final.png" alt="restaurant page" width="550" heigth="550"/>
--- src/home.js
+++
@@ -1,9 +0,0 @@
-let home = document.createElement("div")
-home.id = "home-body"
-    
-let h1 = document.createElement("h1")
-h1.innerHTML = `The <span>PIZZA SHOP</span> takeout & delivery`
-
-home.appendChild(h1)
-
-export {home}
